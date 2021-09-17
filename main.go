861240ba570b3d3c9ff3462a5e9ac5b40c562a1d
Muhammad Zhafran <frans.zhafran@gmail.com>
2021-09-17 15:57:20 +0700

    feat: db init

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"pasarwarga-service-api/migration"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initDB(db *gorm.DB) {
+	dsn := "root:654321@tcp(127.0.0.1:3309)/pasarwarga_article?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func GetConnection() *gorm.DB {
+	return db
+}
 
 func main() {
-	r := gin.Default()
-	
-	initRoutes(r)
-	
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	command := os.Args[1]
+	initDB(db)
+
+	if command == "migrate" {
+		migration.Migrate()
+	} else {
+		r := gin.Default()
+
+		initRoutes(r)
+
+		r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
 }
