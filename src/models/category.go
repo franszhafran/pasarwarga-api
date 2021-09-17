05b5b014258a8dbc9b12c16c18c799acc5cc544b
Muhammad Zhafran <frans.zhafran@gmail.com>
2021-09-17 17:42:36 +0700

    feat(models): added json tag to category

--- src/models/category.go
+++ src/models/category.go
@@ -3,9 +3,9 @@ package models
 import "gorm.io/gorm"
 
 type Category struct {
-	ID           int
-	CategoryName string
-	CategorySlug string
+	ID           int    `json:"id"`
+	CategoryName string `json:"category_name"`
+	CategorySlug string `json:"category_slug"`
 	gorm.Model
-	Articles []Article `gorm:"foreignKey:CategoryID"`
+	Articles []Article `gorm:"foreignKey:CategoryID" json:"articles"`
 }
