cb2d966b1d3939c6771e4e11bba39c8fcfd9962e
Muhammad Zhafran <frans.zhafran@gmail.com>
2021-09-17 17:41:29 +0700

    feat(models): added slug to article

--- src/models/article.go
+++ src/models/article.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Article struct {
 	ID         int      `json:"id"`
 	Title      string   `json:"title"`
+	Slug       string   `json:"slug"`
 	CategoryID int      `json:"category_id"`
 	Category   Category `json:"category"`
 	Content    string   `json:"content"`
